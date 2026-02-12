6b88dcad853c1c06a2eafa9449e6cfc474bf01e3
Amatack <carlosviniciogarcia1997@hotmail.com>
2026-02-12 15:43:41 -0500

    feat: add eCash UI support for send/verify/deposit flows

--- packages/extension/src/ui/action/views/deposit/index.vue
+++ packages/extension/src/ui/action/views/deposit/index.vue
@@ -16,7 +16,8 @@
       <div class="deposit__code">
         <qrcode-vue
           :value="
-            $props.network?.provider == ProviderName.kadena
+            $props.network?.provider == ProviderName.kadena ||
+            $props.network?.provider == ProviderName.ecash
               ? network.displayAddress(account.address)
               : $props.network?.provider == ProviderName.ecash
                 ? ((network as any).cashAddrPrefix || 'ecash') +
